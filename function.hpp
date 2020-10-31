b5562c164261321a6358fadabc1173530546fba3
Bilal-Z <bilalzubairi@hotmail.com>
2020-10-31 20:15:10 +0500

    cleaned directory after merge conflicts

--- function.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef FUNCTION_H
-#define FUNCTION_H
-
-int roundFunction(int ri_less_one, int roundKey_i);
-
-#endif
--- utils.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef HELPER_H
-#define HELPER_H
-#include <string>
-using namespace std;
-bool validateInput(int input, int bits);
-string convert2Hex(string input);
-
-#endif
