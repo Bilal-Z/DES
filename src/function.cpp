b32ba0d7fe21796bf1c2506b7a1d9f28d589596a
Bilal-Z <bilalzubairi@hotmail.com>
2020-11-06 13:28:13 +0500

    implemented round function

--- src/function.cpp
+++ src/function.cpp
@@ -13,7 +13,8 @@ roundFunction::roundFunction(){
 
 bitset<32> roundFunction::function(bitset<32> RiMinusOne, bitset<48> ki)
 {
-	// expand Ri-1 to 48bits
+	bitset<32> output;
+	// expand Ri-1 to 48bits using E(expand Pbox)
 	bitset<48> expandedR;
 	for (auto i = 0; i < 48; i++)
 	{
@@ -25,6 +26,38 @@ bitset<32> roundFunction::function(bitset<32> RiMinusOne, bitset<48> ki)
 	expandedR ^= ki;
 	cout << "expanded Ri-1 XOR Ki: " + expandedR.to_string() << endl;
 
-	// pass through s boxes
-	// pass through straight pbox
+	// pass through S boxes
+	int column;
+	int row;
+	int substitutionNumber;
+	bitset<4> outputBits;
+	int k = 31; // Output bits Counter
+	int j = 0; // Sbox counter
+	for (auto i = 0; i < 48; i+=6)
+	{
+		// take ith block of 6 bits
+		// use first and 6th bit to calculate row number
+		row = (expandedR[47 - i] * 2) + expandedR[47 - i - 5];
+		// use middle 4 for column number
+		column =  (expandedR[47 - i - 1] * 8) + (expandedR[47 - i - 2] * 4) + 
+					(expandedR[47 - i - 3] * 2) + expandedR[47 - i - 4];
+		// get number on that row and column from jth Sbox
+		substitutionNumber = SBoxes[j++][row][column];
+
+		outputBits = bitset<4>(substitutionNumber);
+
+		output[k--] = outputBits[3];
+		output[k--] = outputBits[2];
+		output[k--] = outputBits[1];
+		output[k--] = outputBits[0];
+	}
+	
+	// pass through straight Pbox
+	bitset<32> temp = output;
+	for (auto i = 0; i < 32; i++)
+	{
+		output[31 - i] = temp[31 - (P[i] - 1)];
+	}
+
+	return output;
 }
